a198d6abf9e74b8f5e0d9e354f51af785a4c1390
bnsnku <bnsn.ku@gmail.com>
2014-10-17 17:56:15 -0700

    extra credit except #3
    yep

--- Assignment 1/Code/assignment_1.js
+++ Assignment 1/Code/assignment_1.js
@@ -1,17 +1,123 @@
 var canvas;
 var gl;
-
 var points = [];
-
+var points2 = [];
 var NumTimesToSubdivide = 6;
+var rotationAmount = 0;
+var displayingSierp = true;
 
 window.onload = function init()
 {
     canvas = document.getElementById( "gl-canvas" );
-    
+
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
         
+    displaySierpinski();
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+	
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+	//Set the starting color as black
+	var colorLocation = gl.getUniformLocation(program, "uColor");
+	var uColor = vec4(0.0, 0.0, 0.0, 1.0);
+	if (colorLocation != 1)
+	{
+		gl.uniform4fv(colorLocation, uColor);
+	}
+
+    // Associate out shader variables with our data buffer
+    var matrixLocation = gl.getUniformLocation(program,"uRotation")
+    var rotationMat = rotate(rotationAmount, 0, 0, 1);
+    gl.uniformMatrix4fv(matrixLocation, false, flatten(rotationMat));
+ 
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    //this function waits for keybard events
+	window.onkeypress = function(event)
+	{
+        //alert(event.keyCode);
+        var key = event.keyCode ? event.keyCode : event.which;
+
+        //extra credit #2; use keyboard method to change color
+        //if '1' is pressed, change to yellow.
+        if (key == 49)
+        {
+            var colorLocation = gl.getUniformLocation(program, "uColor");
+            var uColor = vec4(1.0, 1.0, 0.0, 1.0);
+            if (colorLocation != 1)
+            {
+                gl.uniform4fv(colorLocation, uColor);
+            }
+
+            render(); 
+        }
+        
+        //if '2' is pressed, change to black.
+        if (key == 50)
+        {
+            var colorLocation = gl.getUniformLocation(program, "uColor");
+            var uColor = vec4(0.0, 0.0, 0.0, 1.0);
+            if (colorLocation != 1)
+            {
+                gl.uniform4fv(colorLocation, uColor);
+            }
+
+            render(); 
+        }
+
+        //extra credit #3; switch between two display functions
+        if (key == 116)
+        {
+            if (displayingSierp)
+            {
+                //alert("displaying mandelbrot");
+
+                displayMandelbrot();
+                render();
+            }
+
+            else
+            {
+                //alert("displaying sierpinski");
+
+                displaySierpinski();
+                render();
+            }
+        }
+
+        //extra credit #4; rotate 10 deg CW if 'r' is pressed
+        if (key == 114)
+        {
+            rotationAmount += -10;
+            var matrixLocation = gl.getUniformLocation(program,"uRotation")
+            var rotationMat = rotate(rotationAmount, 0, 0, 1);
+            gl.uniformMatrix4fv(matrixLocation, false, flatten(rotationMat));
+
+            render(); 
+        }
+        
+    }
+
+    render();
+	
+};
+
+function displaySierpinski()
+{
+    // clear points in the buffer
+    index = 0;
+    points.splice( index, points.length );
+    
     //
     //  Initialize our data for the Sierpinski Gasket
     //
@@ -26,32 +132,15 @@ window.onload = function init()
 
     divideTriangle( vertices[0], vertices[1], vertices[2],
                     NumTimesToSubdivide);
-
-    //
-    //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
     
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
     // Load the data into the GPU
     
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.DYNAMIC_DRAW );
 
-    // Associate out shader variables with our data buffer
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    render();
-};
+    displayingSierp = true;
+}
 
 function triangle( a, b, c )
 {
@@ -83,6 +172,81 @@ function divideTriangle( a, b, c, count )
         divideTriangle( b, bc, ab, count );
     }
 }
+
+function displayMandelbrot()
+{
+    index = 0;
+    points.splice( index, points.length );
+
+    displayingSierp = false;
+}
+
+
+/// Algorithm for generating and displaying mandelbrot, taken from http://rosettacode.org/wiki/Mandelbrot_set
+// function Mandeliter(cx, cy, maxiter)
+// {
+//   var i;
+//   var x = 0.0;
+//   var y = 0.0;
+//   for (i = 0; i < maxiter && x*x + y*y <= 4; ++i)
+//   {
+//     var tmp = 2*x*y;
+//     x = x*x - y*y + cx;
+//     y = tmp + cy;
+//   }
+//   return i;
+// }
+ 
+// function Mandelbrot()
+// {
+//   var cd = document.getElementById('calcdata');
+//   var xmin = parseFloat(cd.xmin.value);
+//   var xmax = parseFloat(cd.xmax.value);
+//   var ymin = parseFloat(cd.ymin.value);
+//   var ymax = parseFloat(cd.ymax.value);
+//   var iterations = parseInt(cd.iterations.value);
+//   var ctx = document.getElementById('mandelimage').getContext("2d");
+//   var img = ctx.getImageData(0, 0, width, height);
+//   var pix = img.data;
+//   for (var ix = 0; ix < width; ++ix)
+//     for (var iy = 0; iy < height; ++iy)
+//     {
+//       var x = xmin + (xmax-xmin)*ix/(width-1);
+//       var y = ymin + (ymax-ymin)*iy/(height-1);
+//       var i = Mandeliter(x, y, iterations);
+//       var ppos = 4*(900*iy + ix);
+//       if (i == iterations)
+//       {
+//         pix[ppos] = 0;
+//         pix[ppos+1] = 0;
+//         pix[ppos+2] = 0;
+//       }
+//       else
+//       {
+//         var c = 3*Math.log(i)/Math.log(iterations - 1.0);
+//         if (c < 1)
+//         {
+//           pix[ppos] = 255*c;
+//           pix[ppos+1] = 0;
+//           pix[ppos+2] = 0;
+//         }
+//         else if (c < 2)
+//         {
+//           pix[ppos] = 255;
+//           pix[ppos+1] = 255*(c-1);
+//           pix[ppos+2] = 0;
+//         }
+//         else
+//         {
+//           pix[ppos] = 255;
+//           pix[ppos+1] = 255;
+//           pix[ppos+2] = 255*(c-2);
+//         }
+//       }
+//       pix[ppos+3] = 255;
+//     }
+//   ctx.putImageData(img,0,0);
+// }
 
 function render()
 {
---
+++ Assignment 1/assignment1_readme.txt
@@ -0,0 +1,24 @@
+Name: Benson Ku
+ID: 804-038-689
+
+Environment: Sublime Text 2
+Platform: Windows 8
+Browser: Chrome Version 38.0.2125.104 m
+
+I was able to fulfill the basic requirements of the project by using the code from the textbook, including 
+	1. Getting a canvas to display
+	2. implementing shader codes to get primitives onto the screen.
+	3. Implementing the Sierpinski Gasket algorithim using recursive triangles, with (-0.5, -0.5), (0.5, -0.5) and (0.0, 0.5) as the initial seed points of the algorithm
+	4. Code is commented
+
+For the extra credit, I was able to 
+	1. Implement an application based color variable passed to the fragment shader
+	2. Implement a keyboard method to change the color and redisplay
+	3. I was able to switch to a blank screen and switch back to the Sierpinski Gasket, but I was unable to get new points to display
+
+	I found an algorithim for the Mandelbrot fractal, but did not have time to implement it.
+	http://rosettacode.org/wiki/Mandelbrot_set
+	
+	4. Implement a rotation of 10 deg clockwise using the built-in functions in the MV.js file
+
+
